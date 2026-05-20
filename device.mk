5b65e14d481528a8c850c0b9d94eb708e8603199
Safwan Eh S <ehfazinfo@gmail.com>
2026-05-20 17:05:47 +0600

    Update audio package configuration to resolve build errors

--- device.mk
+++ device.mk
@@ -67,18 +67,26 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.3-impl
 
+# Disabling due to build errors, will re-enable once fixed
+# PRODUCT_PACKAGES += \
+#     audio.primary.default \
+#     audio.r_submix.default \
+#     audio.usb.default \
+#     libaudiopreprocessing \
+#     libqcompostprocbundle \
+#     libqcomvisualizer \
+#     libqcomvoiceprocessing \
+#     libtinycompress \
+#     libvolumelistener \
+#     libbatterylistener \
+
 PRODUCT_PACKAGES += \
     audio.primary.default \
     audio.r_submix.default \
     audio.usb.default \
     libaudiopreprocessing \
     libtinycompress \
-    libvolumelistener \
-    libbatterylistener #\
-# Enable when use source kernel
-#     libqcompostprocbundle \
-#     libqcomvisualizer \
-#     libqcomvoiceprocessing \
+    libbatterylistener \
 
 AUDIO_HAL_DIR := hardware/qcom-caf/sm6225/audio/primary-hal
 
