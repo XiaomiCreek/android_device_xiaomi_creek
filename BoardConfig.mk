0f9ddf5276036a909917f12e1516135c92e03bbb
Safwan Eh S <ehfazinfo@gmail.com>
2026-05-20 23:13:34 +0600

    Enable inclusion of DTB in boot images

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,8 +84,9 @@ TARGET_BOARD_KERNEL_HEADERS         := $(KERNEL_PATH)/kernel-headers
 
 # Device Tree Blobs (DTB/DTBO) Formatting
 BOARD_KERNEL_SEPARATED_DTBO         := true
-BOARD_INCLUDE_DTB_IN_BOOTIMG        := false
-BOARD_INCLUDE_DTB_IN_VENDOR_BOOT    := true
+# BOARD_INCLUDE_DTB_IN_BOOTIMG        := false
+# BOARD_INCLUDE_DTB_IN_VENDOR_BOOT    := true
+BOARD_INCLUDE_DTB_IN_BOOTIMG        := true
 
 # DTB & DTBO
 BOARD_USES_DT                       := true
