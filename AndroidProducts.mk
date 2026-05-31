33869c2ab155aae5cf016bb3bc8271710ce5e943
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-05-31 06:54:23 +0400

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,8 +6,3 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_creek.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_creek-trunk_staging-userdebug \
-    lineage_creek-trunk_staging-user \
-    lineage_creek-trunk_staging-eng
