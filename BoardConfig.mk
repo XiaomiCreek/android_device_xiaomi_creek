76b2e35cf02c76eb3924ee22fa5d4ca27df4835d
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-06-27 23:54:17 +0400

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,23 +16,19 @@ KERNEL_PATH := $(DEVICE_PATH)-kernel
 # ==============================================
 
 BUILD_BROKEN_DUP_RULES                       := true
-BUILD_BROKEN_PREBUILT_UI_HAL                 := true
-BUILD_BROKEN_MISSING_PGO_STATS               := true
-BUILD_BROKEN_VINTF_PRODUCT_COPY_FILES        := true
-BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE       := true
+RELAX_USES_LIBRARY_CHECK                     := true
+SOONG_ALLOW_MISSING_DEPENDENCIES             := true
+BUILD_BROKEN_INCORRECT_PARTITION_IMAGES      := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-
-# Dexpreopt Override Controls
-DISABLE_DEXPREOPT_CHECK                      := true
-WITH_DEXPREOPT                               := false
-
-# Allow SOONG missing dependencies
-SOONG_ALLOW_MISSING_DEPENDENCIES := true
 
 # Global LTO
 TARGET_GLOBAL_LTO                            := thin
 TARGET_GLOBAL_OPTIMIZATION                   := O3
 TARGET_GLOBAL_THINLTO                        := true
+
+# Init
+TARGET_RECOVERY_DEVICE_MODULES               := init_creek
+TARGET_RECOVERY_DEVICE_MODULES               += init.recovery.qcom.rc
 
 
 # ==============================================
@@ -40,23 +36,23 @@ TARGET_GLOBAL_THINLTO                        := true
 # ==============================================
 
 # Primary Architecture (64-bit)
-TARGET_ARCH                    := arm64
-TARGET_CPU_ABI                 := arm64-v8a
-TARGET_ARCH_VARIANT            := armv8-a
-TARGET_CPU_VARIANT_RUNTIME     := cortex-a73
+TARGET_ARCH                        := arm64
+TARGET_CPU_ABI                     := arm64-v8a
+TARGET_ARCH_VARIANT                := armv8-a
+TARGET_CPU_VARIANT_RUNTIME         := cortex-a73
 
 # Secondary Architecture (32-bit)
-TARGET_2ND_ARCH                := arm
-TARGET_2ND_CPU_ABI             := armeabi-v7a
-TARGET_2ND_CPU_ABI2            := armeabi
-TARGET_2ND_ARCH_VARIANT        := armv8-a
-TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
+TARGET_2ND_ARCH                    := arm
+TARGET_2ND_CPU_ABI                 := armeabi-v7a
+TARGET_2ND_CPU_ABI2                := armeabi
+TARGET_2ND_ARCH_VARIANT            := armv8-a
+TARGET_2ND_CPU_VARIANT_RUNTIME     := cortex-a53
 
 # Platform
-TARGET_BOARD_SUFFIX            := _515
-TARGET_BOARD_PLATFORM          := bengal
-BOARD_USES_QCOM_HARDWARE       := true
-TARGET_BOOTLOADER_BOARD_NAME   := bengal
+TARGET_BOARD_SUFFIX                := _515
+TARGET_BOARD_PLATFORM              := bengal
+BOARD_USES_QCOM_HARDWARE           := true
+TARGET_BOOTLOADER_BOARD_NAME       := bengal
 
 
 # ==============================================
@@ -89,13 +85,13 @@ BOARD_PREBUILT_DTBOIMAGE            := $(KERNEL_PATH)/dtbs/dtbo.img
 # Basic kernel cmdline
 BOARD_KERNEL_CMDLINE := \
     console=ttyMSM0,115200n8 \
-    androidboot.hardware=qcom \
     androidboot.selinux=permissive \
+    disable_dma32=on \
     rcu_nocbs=all \
+    rcutree.enable_rcu_lazy=1
     rcu_normal=1 \
     rcu_expedited=1 \
-    kasan=off \
-    mtdoops.fingerprint=creek:13/OS3.0.10.0.WBOMIXM:user
+    kasan=off
 #    qcom_geni_serial.con_enabled=0 # uncomment when device boots successfully.
 
 BOARD_BOOTCONFIG := \
@@ -115,6 +111,9 @@ BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE         :=  $(KERNEL_PATH)/vendor_dlk
 BOARD_SYSTEM_KERNEL_MODULES_LOAD                   := $(strip $(shell cat $(KERNEL_PATH)/system_dlkm/modules.load))
 BOARD_SYSTEM_KERNEL_MODULES_BLOCKLIST_FILE         :=  $(KERNEL_PATH)/system_dlkm/modules.blocklist
 
+# Modules for the rest of the Hardware (RECOVERY)
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD  := $(strip $(shell cat $(KERNEL_PATH)/vendor_ramdisk/modules.load.recovery))
+
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(KERNEL_PATH)/system_dlkm/,$(TARGET_COPY_OUT_SYSTEM_DLKM)/lib/modules/5.15.178) \
     $(call find-copy-subdir-files,*,$(KERNEL_PATH)/vendor_dlkm/,$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules) \
@@ -127,7 +126,6 @@ PRODUCT_COPY_FILES += \
 
 # Boot Header (GKI 2.0 Standards)
 BOARD_USES_GENERIC_KERNEL_IMAGE    := true
-BOARD_COPY_BOOT_IMAGE_TO_INIT_BOOT := true
 
 BOARD_BOOT_HEADER_VERSION          := 4
 BOARD_MKBOOTIMG_ARGS               += --header_version $(BOARD_BOOT_HEADER_VERSION)
@@ -135,13 +133,9 @@ BOARD_MKBOOTIMG_ARGS               += --header_version $(BOARD_BOOT_HEADER_VERSI
 BOARD_INIT_BOOT_HEADER_VERSION     := 4
 BOARD_MKBOOTIMG_INIT_ARGS          += --header_version $(BOARD_INIT_BOOT_HEADER_VERSION)
 
-BOARD_RECOVERY_HEADER_VERSION      := 4
-BOARD_RECOVERY_MKBOOTIMG_ARGS      := --header_version $(BOARD_RECOVERY_HEADER_VERSION)
-
 # Partition Memory Offsets
 BOARD_DTB_OFFSET                   := 0x01f00000
 BOARD_KERNEL_BASE                  := 0x00000000
-BOARD_VENDOR_BASE                  := 0x00000000
 BOARD_TAGS_OFFSET                  := 0x00000100
 BOARD_KERNEL_OFFSET                := 0x00008000
 BOARD_RAMDISK_OFFSET               := 0x01000000
@@ -153,27 +147,27 @@ BOARD_KERNEL_PAGESIZE              := 4096
 # ==============================================
 
 # Partition Target Maps
-TARGET_COPY_OUT_ODM                         := odm
-TARGET_COPY_OUT_VENDOR                      := vendor
-TARGET_COPY_OUT_PRODUCT                     := product
-TARGET_COPY_OUT_SYSTEM_EXT                  := system_ext
-TARGET_COPY_OUT_VENDOR_DLKM                 := vendor_dlkm
-TARGET_COPY_OUT_SYSTEM_DLKM                 := system_dlkm
+TARGET_COPY_OUT_ODM                            := odm
+TARGET_COPY_OUT_VENDOR                         := vendor
+TARGET_COPY_OUT_PRODUCT                        := product
+TARGET_COPY_OUT_SYSTEM_EXT                     := system_ext
+TARGET_COPY_OUT_VENDOR_DLKM                    := vendor_dlkm
+TARGET_COPY_OUT_SYSTEM_DLKM                    := system_dlkm
 
 # Physical Image Sizes
-BOARD_FLASH_BLOCK_SIZE                      := 262144
-BOARD_DTBOIMG_PARTITION_SIZE                := 0x01800000
-BOARD_BOOTIMAGE_PARTITION_SIZE              := 0x06000000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE          := 0x06400000
-BOARD_INIT_BOOT_IMAGE_PARTITION_SIZE        := 0x00800000
-BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE       := 0x06000000
+BOARD_FLASH_BLOCK_SIZE                         := 262144
+BOARD_DTBOIMG_PARTITION_SIZE                   := 8388608
+BOARD_BOOTIMAGE_PARTITION_SIZE                 := 100663296
+BOARD_RECOVERYIMAGE_PARTITION_SIZE             := 104857600
+BOARD_INIT_BOOT_IMAGE_PARTITION_SIZE           := 8388608
+BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE          := 100663296
 
 # Super & Dynamic Partition Framework Config
-BOARD_SUPER_PARTITION_SIZE                  := 9126805504
-BOARD_SUPER_PARTITION_GROUPS                := qti_dynamic_partitions
-BOARD_QTI_DYNAMIC_PARTITIONS_SIZE           := 9122611200
-BOARD_SUPER_PARTITION_METADATA_DEVICE       := super
-BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := \
+BOARD_SUPER_PARTITION_SIZE                     := 9126805504
+BOARD_SUPER_PARTITION_GROUPS                   := xiaomi_dynamic_partitions
+BOARD_XIAOMI_DYNAMIC_PARTITIONS_SIZE           := 9122611200
+BOARD_SUPER_PARTITION_METADATA_DEVICE          := super
+BOARD_XIAOMI_DYNAMIC_PARTITIONS_PARTITION_LIST := \
     odm \
     product \
     system \
@@ -188,7 +182,7 @@ BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := \
 # ==============================================
 
 TARGET_FS_CONFIG_GEN                    := $(DEVICE_PATH)/configs/config.fs
-BOARD_HAS_REMOVABLE_STORAGE             := true
+#BOARD_HAS_REMOVABLE_STORAGE             := true
 
 # Filesystem Types Selection
 BOARD_ODMIMAGE_FILE_SYSTEM_TYPE         := erofs
@@ -276,11 +270,6 @@ BOARD_AVB_VENDOR_BOOT_KEY_PATH                    := external/avb/test/data/test
 BOARD_AVB_VENDOR_BOOT_ROLLBACK_INDEX              := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_VENDOR_BOOT_ROLLBACK_INDEX_LOCATION     := 5
 
-BOARD_AVB_INIT_BOOT_ALGORITHM                     := SHA256_RSA4096
-BOARD_AVB_INIT_BOOT_KEY_PATH                      := external/avb/test/data/testkey_rsa4096.pem
-BOARD_AVB_INIT_BOOT_ROLLBACK_INDEX                := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
-BOARD_AVB_INIT_BOOT_ROLLBACK_INDEX_LOCATION       := 6
-
 # VBMETA System Configuration
 BOARD_AVB_VBMETA_SYSTEM                           := product system system_dlkm system_ext
 BOARD_AVB_VBMETA_SYSTEM_ALGORITHM                 := SHA256_RSA4096
@@ -300,23 +289,21 @@ BOARD_AVB_ODM_ADD_HASHTREE_FOOTER_ARGS            += --hash_algorithm sha256
 # ==============================================
 
 # Inherit Qualcomm and Xiaomi common policies
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr/sm6225/SEPolicy.mk
 include device/xiaomi/sepolicy/SEPolicy.mk
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS                        += $(DEVICE_PATH)/sepolicy/vendor
-# SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS                   += $(DEVICE_PATH)/sepolicy/public
-# SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS                  += $(DEVICE_PATH)/sepolicy/private
-
-# Ignore neverallow violations for initial bring-up on creek
-SELINUX_IGNORE_NEVERALLOWS                        := true
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS                  += $(DEVICE_PATH)/sepolicy/private
 
 
 # ==============================================
 # System Parameters, Build Contexts, Overrides
 # ==============================================
 
-# Soong Project Hooks
+# Boot control
 $(call soong_config_set, ufsbsg, ufsframework, bsg)
+
 $(call soong_config_set,libinit,vendor_init_lib,//$(DEVICE_PATH):init_creek)
 $(call soong_config_set_bool, camera, override_format_from_reserved, true)
 
@@ -332,11 +319,11 @@ TARGET_RECOVERY_PIXEL_FORMAT               := RGBX_8888
 BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE   := true
 
 # Properties
-TARGET_ODM_PROP                            := $(DEVICE_PATH)/configs/properties/odm.prop
-TARGET_SYSTEM_PROP                         := $(DEVICE_PATH)/configs/properties/system.prop
-TARGET_VENDOR_PROP                         := $(DEVICE_PATH)/configs/properties/vendor.prop
-TARGET_PRODUCT_PROP                        := $(DEVICE_PATH)/configs/properties/product.prop
-TARGET_SYSTEM_EXT_PROP                     := $(DEVICE_PATH)/configs/properties/system_ext.prop
+TARGET_ODM_PROP                            += $(DEVICE_PATH)/configs/properties/odm.prop
+TARGET_SYSTEM_PROP                         += $(DEVICE_PATH)/configs/properties/system.prop
+TARGET_VENDOR_PROP                         += $(DEVICE_PATH)/configs/properties/vendor.prop
+TARGET_PRODUCT_PROP                        += $(DEVICE_PATH)/configs/properties/product.prop
+TARGET_SYSTEM_EXT_PROP                     += $(DEVICE_PATH)/configs/properties/system_ext.prop
 
 # VINTF Compatibility Matrices & Interface Verification
 DEVICE_MANIFEST_FILE += \
@@ -348,12 +335,9 @@ DEVICE_FRAMEWORK_MANIFEST_FILE += \
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    hardware/lineage/interfaces/compatibility_matrices/compatibility_matrix.lineage.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 
 DEVICE_MATRIX_FILE += \
-    $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml \
     hardware/qcom-caf/common/compatibility_matrix.xml
 
 
@@ -362,7 +346,7 @@ DEVICE_MATRIX_FILE += \
 # ==========================================================
 
 # Display Graphics
-TARGET_SCREEN_DENSITY                          := 420
+TARGET_SCREEN_DENSITY                          := 450
 TARGET_HAS_HDR_DISPLAY                         := false
 TARGET_HAS_WIDE_COLOR_DISPLAY                  := true
 TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE        := true
@@ -397,3 +381,6 @@ WIFI_HIDL_FEATURE_DUAL_INTERFACE               := true
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB               := $(BOARD_HOSTAPD_PRIVATE_LIB)
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB_EVENT         := "ON"
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY  := true
+
+# Fix: Create directory KERNEL_OBJ/usr for some modules
+$(shell mkdir -p $(OUT_DIR)/target/product/$(TARGET_DEVICE)/obj/KERNEL_OBJ/usr)
