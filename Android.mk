67f23e5afc9b6a313da472eae1e443dd5e994a75
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-06-27 21:39:45 +0400

    Modify TARGET_DEVICE conditional in Android.mk
    Change conditional check for TARGET_DEVICE to use 'ifeq' instead of 'ifneq'.

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter creek sapphire topaz xun,$(TARGET_DEVICE)),)
+ifeq ($(TARGET_DEVICE),creek)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 # A/B builds require us to create the mount points at compile time.
