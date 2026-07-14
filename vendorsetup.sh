99296f7553d1cb5b1880fa2c22ad3c9448898bbf
Xiaomi Creek <ask.in.uae@gmail.com>
2026-07-14 21:16:49 +0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -34,6 +34,9 @@ git clone --depth 1 -b lineage-23.2 https://github.com/sapphire-sm6225/android_v
 rm -rf hardware/dolby
 git clone --depth 1 -b 16 https://github.com/project-creek/hardware_dolby.git hardware/dolby
 
+rm -rf packages/apps/FastCharge
+git clone --depth 1 -b 16-qpr2 https://github.com/project-creek/packages_apps_FastCharge.git packages/apps/FastCharge
+
 echo "============================"
 echo "Cloning HALs completed"
 echo "============================"
