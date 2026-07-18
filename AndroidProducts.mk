1ce60a99103807319c50bb1d39d68f766e5f4fb6
Xiaomi Creek <ask.in.uae@gmail.com>
2026-07-18 13:24:14 +0400

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,12 @@
 #
-# Copyright (C) 2026 The LineageOS Project
-#
+# SPDX-FileCopyrightText: The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_creek.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_creek-user \
+    lineage_creek-userdebug \
+    lineage_creek-eng
