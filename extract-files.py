e95b6d1bacd561718034bb5adc8174a0789cf677
XiaomiCreek <xiaomicreek@github.com>
2026-08-03 10:17:58 +0200

    creek: Add libtinyxml fixup for displayfeature

--- extract-files.py
+++ extract-files.py
@@ -31,7 +31,8 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/bin/hw/android.hardware.security.keymint-service-qti', 'vendor/lib64/libqtikeymint.so'): blob_fixup()
         .add_needed('android.hardware.security.rkp-V3-ndk.so'),
     ('vendor/lib64/hw/displayfeature.default.so'): blob_fixup()
-        .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
+        .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so')
+        .replace_needed('libtinyxml.so', 'libtinyxml2_1.so'),
     'libcodec2_hidl@1.0.so': blob_fixup()
         .add_needed('libshim.so'),
     'vendor/etc/qcril_database/upgrade/config/6.0_config.sql': blob_fixup()
