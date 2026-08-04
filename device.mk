9877a779649dd9da18808c7fd046af16acfaaebf
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-08-04 18:46:04 +0400

    Add USB service and update product packages

--- device.mk
+++ device.mk
@@ -627,7 +627,8 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.qti
+    android.hardware.usb-service.qti \
+    android.hardware.usb@1.2
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
