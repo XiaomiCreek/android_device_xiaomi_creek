ba249793e91ee2e9b59322197e35d3c9bc12d10c
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-08-12 12:44:19 +0400

    Add Xiaomi framework compatibility matrix file

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -324,7 +324,8 @@ DEVICE_FRAMEWORK_MANIFEST_FILE += \
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 
 DEVICE_MATRIX_FILE += \
     hardware/qcom-caf/common/compatibility_matrix.xml
